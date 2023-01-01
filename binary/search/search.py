a1c72435bd730a4e826fc11c3983735885c16e83
alex narayan <alex@alexandarnarayan.com>
2023-01-01 13:39:57 +0100

    rename function to just exists because that's all it does, an existence check

--- binary/search/search.py
+++ binary/search/search.py
@@ -6,14 +6,14 @@ index: is the index in the original list of the target
 return None if not found
 """
 
-def BinarySearch(l: list, target)->bool:
+def exists(l: list, target)->bool:
     midpoint: int = len(l) // 2
     if target > l[midpoint]:
         # print(f"{target=} is larger than the {midpoint=}, so now the list is: {l[midpoint:]}")
-        return BinarySearch(l=l[midpoint:], target=target)
+        return exists(l=l[midpoint:], target=target)
     if target < l[midpoint]:
         # print(f"{target=} is smaller than the {midpoint=}, so now the list is: {l[0:midpoint]}")
-        return BinarySearch(l=l[0:midpoint], target=target) 
+        return exists(l=l[0:midpoint], target=target) 
     elif target == l[midpoint]:
         # print(f"{target=} and {midpoint=}, so now the list is: {l[midpoint:]}")
         return True
@@ -21,4 +21,17 @@ def BinarySearch(l: list, target)->bool:
         return False
 
 
-     
+def BinarySearch(l: list, target)->int|None:
+    index: int = -1 
+    if len(l) == 1:
+        return 0
+    midpoint: int = len(l) // 2
+
+
+
+
+
+
+
+    
+
